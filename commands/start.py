29ab9279b33467c960c4eb32ffc9d12b0392d012
Farhad Uneci <farhaduneci@gmail.com>
2024-03-05 20:24:53 +0330

    ✏️  Start command typo

--- commands/start.py
+++ commands/start.py
@@ -7,7 +7,7 @@ from utils.decorators import send_action
 logger = getLogger(__name__)
 
 START_TEXT = """
-Hello and welcome herei. 👋
+Hello and welcome here. 👋
 
 You can send me the present, past and past participle of a verb and I will tell you what the other forms are.
 """
