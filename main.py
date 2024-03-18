9fb6255e1ae14651e79101bbc9d54a3fab02a44d
Farhad Uneci <farhaduneci@gmail.com>
2024-03-18 13:02:00 +0330

    ➕ Sentry support

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sentry_sdk
+
 from telegram.ext import Application
 from telegram.ext import CommandHandler
 from telegram.ext import filters
@@ -8,10 +10,12 @@ from commands.maintenance import maintenance
 from commands.start import start
 from commands.conjugate import conjugate
 from configurations import settings
-from configurations.settings import IS_MAINTENANCE
+from configurations.settings import IS_MAINTENANCE, SENTRY_DSN
 from utils import logger
 
+
 if __name__ == "__main__":
+    sentry_sdk.init(dsn=SENTRY_DSN)
     logger.init_logger(f"logs/bot.log")
 
     persistence = PicklePersistence(filepath="conversation_states.pickle")
